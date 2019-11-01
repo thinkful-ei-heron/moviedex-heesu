ff029669cae1494d5674c382961761afc3bc54da
Heesu Kang <hskang135@gmail.com>
2019-11-01 12:56:32 -0400

    fix some

--- server.js
+++ server.js
@@ -6,35 +6,40 @@ const helmet = require('helmet');
 
 const app = express();
 
+const store = require('./movies-data-small');
+const API_TOKEN = process.env.API_TOKEN;
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+
 app.use(morgan(morganSetting));
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
+app.use(validateBearer);
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization') || '';
-
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' })
-  };
+function validateBearer(req, res, next) {
+  const authVal = req.get('Authorization') || '';
+  if (!authVal.startsWith('Bearer ')) {
+    return res.status(400).json({ error: 'Authorization token not found' });
+  }
+  const token = authVal.split(' ')[1];
+  if (token !== API_TOKEN) {
+    return res.status(401).json({ error: 'Token is invalid' });
+  }
   next();
-});
+}
 
 app.use((error, req, res, next) => {
-  let response
+  let response;
   if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' }}
+    response = { error: { message: 'server error' } };
   } else {
-    response = { error }
+    response = { error };
   }
-  res.status(500).json(response)
+  res.status(500).json(response);
 });
 
 app.get('/movie', (req, res) => {
   let movies = store;
-  const {genre, country, avg_vote} = req.query;
+  const { genre, country, avg_vote } = req.query;
 
   if (genre) {
     movies = movies.filter((app) =>
@@ -56,4 +61,5 @@ app.get('/movie', (req, res) => {
 });
 
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {});
